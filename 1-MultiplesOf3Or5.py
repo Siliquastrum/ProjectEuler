016c49f112043443be40b344b154d6d08b64e2cd
Enes Bekdemir <42enesbekdemir@gmail.com>
2022-11-05 15:19:23 +0300

    Multiples of 3 or 5 Solution

---
+++ 1-MultiplesOf3Or5.py
@@ -0,0 +1,40 @@
+__author__ = "Enes Bekdemir"
+__date__ = "05 Nov 2022"
+
+""" Problem
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+# Solution 1:
+f = lambda a: a * (a + 1) // 2
+
+def solution(n):
+    n -= 1
+    return 3 * f(n//3) + 5 * f(n//5) - 15 * f(n//15)
+
+while inp != 0:
+    inp = int(input())
+    print(solution(inp))
+
+
+
+
+
+""" Pseudo Solution
+for a0 in range(int(input().strip())):
+    x = 0
+    for num in range(1, int(input().strip())):
+        if num%3 == 0 or num%5 == 0: x += num 
+    print(x)
+""" 
+
+
+
+
+
+""" Pseudo Solution
+for a0 in range(int(input().strip())):
+    print(sum([num for num in range(1, int(input().strip())) if num%3 == 0 or num%5 == 0]))
+""" 
+    
