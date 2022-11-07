06e8cbd3ccf60650aec6c380b38ac12993e727a4
Enes Bekdemir <42enesbekdemir@gmail.com>
2022-11-07 13:08:52 +0300

    10.001st Prime Solution

---
+++ 7-10001stPrime.py
@@ -0,0 +1,54 @@
+__author__ = "Enes Bekdemir"
+__date__ = "07 Nov 2022"
+
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10001st prime number?
+Answer: 104743
+"""
+
+# # 2 # My Solution in HackerRank:
+# https://www.hackerrank.com/contests/projecteuler/challenges/euler007/problem
+prime = [0, 2, 3]
+def isPrime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0: return False
+    return True
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    try: 
+        print(prime[n])
+    except:
+        ln = len(prime)
+        ind = ln - 1
+        num = prime[ind]
+        while ln <= n+1:
+            num += 2
+            if isPrime(num): 
+                prime.append(num)
+                ln += 1
+        print(prime[n])
+
+
+
+
+
+""" # 1 # Pseudo Solution:
+#*#*# from 3-LargestPrimeFactor.py, the author is "Enes Bekdemir" so I :).
+import math
+
+def isPrime(n): # Finds whether n is prime or not.
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0: return False
+    return True
+#*#*#
+primest = 0
+n = int(input())
+prime = 2
+while n != primest:
+    if isPrime(prime): primest += 1
+    prime += 1
+print(prime - 1)
+"""
